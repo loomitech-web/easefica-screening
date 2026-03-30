5feccb7d360bbd6b21938c146f5090ac985caa7c
MaxThunderbolt <maxvanderwalt3@gmail.com>
2026-03-30 13:16:11 +0200

    Add Vuetify integration and update dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-app>
+    <v-main>
+      <h1>You did it!</h1>
+      <p>
+        Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+        documentation
+      </p>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped></style>
